6902b2795cc8aad0c8e77944eb27a259768c4997
ShaneQful <shane_quigley@hotmail.com>
2013-01-12 18:08:56 +0000

    Markdown preview

--- js/markdown_helper.js
+++ js/markdown_helper.js
@@ -1,3 +1,25 @@
 $(function() {
+	var md = require( "markdown" ).markdown;
 	$("#MarkdownHelperAccord").accordion();
+	$("#pre").button();
+	$("#save").button();
+	$( "#dialog-message" ).dialog({
+		modal: true,
+		autoOpen: false,
+		show: "blind",
+		hide: "blind",
+		buttons: {
+			Ok: function() {
+				$( this ).dialog( "close" );
+			}
+		}
+	});
+	$("#pre").click(function(){
+		var output = md.toHTML($("#markdown").val());
+		$("#dialog-message").html(output);
+		$("#dialog-message").dialog("open");
+	});
+	$("#save").click(function(){
+		//Move to local storage and store in dom
+	});
 });
