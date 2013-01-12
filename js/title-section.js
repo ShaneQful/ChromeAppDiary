aae45add865458b2424689b74fdd01dbeac17936
ShaneQful <shane_quigley@hotmail.com>
2013-01-12 16:06:05 +0000

    Merge Joao's work

---
+++ js/title-section.js
@@ -0,0 +1,20 @@
+var title = document.getElementById('title');
+
+addEvent(title, 'blur', function () {
+  // lame that we're hooking the blur event
+  localStorage.setItem('title', this.innerHTML);
+  document.designMode = 'off';
+});
+
+addEvent(title, 'focus', function () {
+  document.designMode = 'on';
+});
+
+addEvent(document.getElementById('clear'), 'click', function () {
+  localStorage.clear();
+  window.location = window.location; // refresh
+});
+
+if (localStorage.getItem('title')) {
+  title.innerHTML = localStorage.getItem('title');
+}
