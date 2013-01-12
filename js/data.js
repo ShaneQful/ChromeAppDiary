833ba4b8d0f0f7bcc755123a34e7a0c35738067d
Joao Oliveira <joao@nicolauoliveira.com>
2013-01-12 17:23:26 +0000

    improvements

---
+++ js/data.js
@@ -0,0 +1,17 @@
+data = [
+	{
+		date: new Date(2013, 1, 24),
+		title: 'New entry',
+		body: 'Your text here...'
+	},
+	{
+		date: new Date(2013, 1, 23),
+		title: 'New entry 2',
+		body: 'Your text here...'
+	},
+	{
+		date: new Date(2013, 1, 22),
+		title: 'New entry 3',
+		body: 'Your text here...'
+	},
+]
--- js/entry-list.js
+++ js/entry-list.js
@@ -2,4 +2,13 @@
 
 $(function() {
 	$( "#entry-list" ).selectable();
+	$( "#newBut" ).button();
+	$( "#deleteBut" ).button();
+
+	$(data).each(function() {
+		
+	});
+
 });
+
+
